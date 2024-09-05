a7f4f6ed1ad4db60c658fca45577aa5ecaaef788
sanja911 <avi.sanja@gmail.com>
2024-09-05 16:43:42 +0700

    Fix error graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,7 @@ import { CommentsModule } from './modules/comments/comments.module';
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      typePaths: ['./**/*.graphql'],
+      typePaths: ['./src/**/*.graphql'],
       playground: true,
     }),
     TypeOrmModule.forRoot(databaseConfig),
